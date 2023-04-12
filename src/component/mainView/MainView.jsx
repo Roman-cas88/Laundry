16c050fa9dd0c9fecd087e7b2a5091f3baa13c1d
Roman-cas88 <orekhovskiy.roman@gmail.com>
2023-04-12 14:27:02 +0200

    change day

--- src/component/mainView/MainView.jsx
+++ src/component/mainView/MainView.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import './mainView.css'
 
 export const MainView = () => {
-  const date = new Date()
+  const currentDate = new Date()
+  const [date, setDate] = useState(currentDate)
+  const tomorrowFunction = () => {
+    let tomorrow =  new Date()
+    tomorrow.setDate(date.getDate() + 1)
+    setDate(tomorrow)
+  }
+  const yesterdayFunction = () => {
+    let yesterday =  new Date()
+    yesterday.setDate(date.getDate() - 1)
+    setDate(yesterday)
+  }
+
+
   const day = date.getDate()
   const month = `${date.getMonth() + 1}`.padStart(2, "0")
   const year = date.getFullYear()
@@ -11,19 +24,21 @@ export const MainView = () => {
   const dayOfWeek = week[date.getDay()]
   const reservTime = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22]
 
+
   const handleClick = (e) => {
     e.target.style.backgroundColor = 'green'
+    console.log();
   }
 
   return (
     <Container className='mainView'>
         <div  className='date'>
-          <button className='butLeft'>{'<'}</button>
+          <button onClick={yesterdayFunction} className='butLeft'>{'<'}</button>
           <div>
             {day}.{month}.{year} <br/>
             {dayOfWeek}
           </div>
-          <button className='butRight'>{'>'}</button>
+          <button onClick={tomorrowFunction} className='butRight'>{'>'}</button>
         </div>
             {reservTime.map((time) => (
                 <div key={time} onClick={handleClick} className='time'>{time}:00</div>
