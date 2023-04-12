4a485b14a9f37b93ee4236388a8b50f99c61943a
Roman-cas88 <orekhovskiy.roman@gmail.com>
2023-04-12 10:46:07 +0200

    get Date / some SCC

--- src/component/mainView/MainView.jsx
+++ src/component/mainView/MainView.jsx
@@ -3,14 +3,26 @@ import { Container } from 'react-bootstrap'
 import './mainView.css'
 
 export const MainView = () => {
-    const reservTime = ['6.00', '7.00', '8.00', '9.00', '10.00', '11.00', '12.00',
-     '13.00', '14.00', '15.00', '16.00', '17.00', '18.00', '19.00',
-    '20.00', '21.00', '22.00']
+  const date = new Date()
+  const day = date.getDate()
+  const month = `${date.getMonth() + 1}`.padStart(2, "0")
+  const year = date.getFullYear()
+  const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  const dayOfWeek = week[date.getDay()]
+  const reservTime = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22]
+
   return (
     <Container className='mainView'>
-        <div style={{height:'6%', display:'flex', justifyContent:'center', alignItems:'center'}} className='text-center bg-primary text-white border border-dark'>Date</div>
+        <div  className='date'>
+          <button className='butLeft'>{'<'}</button>
+          <div>
+            {day}.{month}.{year} <br/>
+            {dayOfWeek}
+          </div>
+          <button className='butRight'>{'>'}</button>
+        </div>
             {reservTime.map((time) => (
-                <div key={time} style={{height:'5%', textAlign:'center'}} className='border-2 text-center bg-secondary text-white border border-dark'>{time}</div>
+                <div key={time} className='time'>{time}:00</div>
             ))}
     </Container>
   )
