59946917da6320e813a10bd3df3cd60bd9f26d2f
Zhanna Shchanova <Zhanna.Shchanova@gmail.com>
2023-04-22 20:50:27 +0300

    added Registration and router

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -3,9 +3,16 @@ import { useForm } from "react-hook-form";
 import { Container, Button } from "react-bootstrap/";
 import Form from 'react-bootstrap/Form';
 import style from "./Login.module.css"
-
+import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
+    const navigate = useNavigate();
+    const onSubmit = (data) => {
+        console.log(data);
+        // login(data);
+        navigate("/main")
+    };
 const {
     register,
     handleSubmit,
@@ -20,57 +27,11 @@ const {
 );
 const password = useRef({});
 password.current = watch("password", "");
-const onSubmit = data => console.log(data);
 
 return (
     <Container style={{maxWidth: '500px'}}>
     <Form className='border p-3 mb-5 ' onSubmit={handleSubmit(onSubmit)} >
         <h3 >Login</h3>
-        <Form.Group className="mb-3" id="firstName">
-            <Form.Control
-                type="text"
-                placeholder="First name"
-                {...register("firstName", {
-                    required: "Please enter your First name",
-                    pattern: {
-                        value: /^[^0-9]/,
-                        message: "Please enter a valid name"
-                    }
-                })}
-            />
-            {errors.firstName && (
-                <p className={style.errorMsg}>{errors.firstName.message}</p> 
-            )}
-        </Form.Group>
-        <Form.Group className="mb-3" id="lastName">
-            <Form.Control
-                type="text"
-                placeholder="Last name"
-                {...register("lastName", {
-                    required: "Please enter your Last name",
-                    pattern: {
-                        value: /^[^0-9]/,
-                        message: "Please enter a valid name"
-                    }
-                })}
-            />
-            {errors.lastName && (
-                <p className={style.errorMsg}>{errors.lastName.message}</p> 
-            )}
-        </Form.Group>
-        <Form.Group className="mb-3" id="appartmentNum">
-            <Form.Control
-                type="number"
-                placeholder="Appartment number"
-                {...register("appartmentNum", {
-                    required: "Please enter your appartment number."
-                })}
-            />
-            {errors.appartmentNum && (
-                <p className={style.errorMsg}>{errors.appartmentNum.message}</p>  
-            )}
-        </Form.Group>
-
         <Form.Group className="mb-3" controlId="email">
             <Form.Control
                 type="email"
@@ -102,24 +63,17 @@ return (
                 <p className={style.errorMsg}>{errors.password.message}</p>
             )}
         </Form.Group>
-        <Form.Group className="mb-3" controlId="confirmPassword">
-                    <Form.Control
-                        type="password"
-                        placeholder="Re-enter your password"
-                        autoComplete="on"
-                        {...register("confirmPassword", {
-                            validate: (value) =>
-                                value === password.current || "The passwords do not match"
-                        })}
-                    />
-                    <Form.Text className="text-muted">
-                        Your password must contain at least 6 characters. <br />
-                    </Form.Text>
-                    {errors.confirmPassword && (
-                        <p className={style.errorMsg}>{errors.confirmPassword.message}</p>
-                    )}
-        </Form.Group>
-        <Button variant="primary" type="submit"> Register now </ Button >
+            <Form.Check className="mb-3" controlId="rememberme"
+            type = "checkbox"
+            id= ""
+            label = "Remember me"
+            />
+        <Button className="mb-3 w-100" variant="primary" type="submit"> Log in </ Button >
+        <div>
+            <Link to="/registration">
+            <Button className="mb-3 w-100" variant="outline-primary"> Register now </ Button >
+            </Link>
+        </div>
         </Form>
     </ Container>
 )
